1e8447d4bfd5ffad8e04d865d159ceffe98db408
budde-lukas <lu.budde@icloud.com>
2025-02-14 22:47:14 +0100

    Update funktionen1.R
    Ich habe Aufgabe 2.5 hinzugefügt

--- funktionen1.R
+++ funktionen1.R
@@ -76,6 +76,37 @@ bivariat_kategorial <- function(var1, var2) {
 
 #Eine Funktion, die eine geeignete Visualisierung von drei oder vier kategorialen Variablen erstellt
 
+visualize_categorical <- function(data, var1, var2, var3, var4 = NULL) {
+  # Überprüfen, ob die vierte Variable angegeben wurde
+  if (is.null(var4)) {
+    # Kontingenztabelle für drei Variablen
+    contingency_table <- table(data[[var1]], data[[var2]], data[[var3]])
+    
+    # Mosaikplot für drei Variablen
+    mosaicplot(contingency_table, 
+               main = paste("Visualisierung von", var1, "nach", var2, "und", var3),
+               xlab = var1,  
+               ylab = paste(var2, "und", var3),
+               color = c("red", "blue", "green"),
+               las = 1,  # Horizontale Achsenbeschriftung für bessere Lesbarkeit
+               cex.axis = 0.8,  # Kleinere Schrift für Achsenbeschriftungen
+               border = "gray")  # Dezentere Linien für bessere Optik
+  } else {
+    # Kontingenztabelle für vier Variablen
+    contingency_table <- table(data[[var1]], data[[var2]], data[[var3]], data[[var4]])
+    
+    # Mosaikplot für vier Variablen
+    mosaicplot(contingency_table, 
+               main = paste("Visualisierung von", var1, "nach", var2, var3, "und", var4),
+               xlab = var1,  
+               ylab = paste(var2, var3, "und", var4),
+               color = c("red", "blue", "green", "orange"),
+               las = 1,  
+               cex.axis = 0.8,
+               border = "gray")
+  }
+}
+
 
 
 
